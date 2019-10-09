f3a1830a0af46318411863d7a791086ed0544f2e
volodymyr <vblagoi@gmail.com>
2019-10-09 21:53:05 +0300

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
